a8e519b906861d4a2b8e1ca5139491f6c7a9a43b
MunibQazi12 <munibqazi770@gmail.com>
2024-06-21 19:46:43 +0500

    Deisgned Medical & Dentist Dashboard

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let active = false
 function myFunction() {
-    console.log("issue not found")
     let sidebar = document.getElementById("dashboard-sidebar");
     let dashboard = document.getElementById("dashboard-content")
     active = !active
@@ -12,3 +11,31 @@ function myFunction() {
         dashboard.classList.remove('content-screen')
     }
 }
+
+// ==============================================================
+// Side Bar Dropdown Script 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownToggle = document.querySelector('.dropdown-toggle');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    const dropdownArrow = document.getElementById('dropdown-arrow');
+
+    dropdownToggle.addEventListener('click', function () {
+        if (dropdownMenu.style.maxHeight === '0px' || dropdownMenu.style.maxHeight === '') {
+            const scrollHeight = dropdownMenu.scrollHeight;
+            dropdownMenu.style.maxHeight = `${scrollHeight}px`;
+            dropdownArrow.classList.add('-rotate-90');
+        } else {
+            dropdownMenu.style.maxHeight = '0px';
+            dropdownArrow.classList.remove('-rotate-90');
+        }
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.style.maxHeight = '0px';
+            dropdownArrow.classList.remove('-rotate-90');
+        }
+    });
+});
+// ============================================================== 
